1448cd2ace79bda0c793803bc50dd748f2296af9
Kevyn FLORESDACOSTA <kev.floresdacosta@ecoles-epsi.net>
2025-01-19 21:57:17 +0100

    Remplacement du composant Header par le nouveau composant Navbar et suppression du fichier Button.js

---
+++ src/Components/Common/Navbar/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default function Navbar() {
+  return (
+    <>
+      <div className='h-24 flex items-center justify-center space-x-16 font-[#081C15] text-2xl'>
+        <div>Profil</div>
+        <div>Tableau de synthèse</div>
+        <div>Compétences</div>
+        <div>Expériences</div>
+        <div>Veille</div>
+        <div>Projets</div>
+        <div>Contact</div>
+        <img src={`${process.env.PUBLIC_URL}/moon.png`} alt='toogle to dark theme'></img>
+        <hr></hr>
+      </div>
+    </>
+  );
+};
--- src/pages/Header/Header.js
+++ src/pages/Header/Header.js
@@ -1,19 +1,10 @@
 import React from 'react';
+import Navbar from '../../Components/Common/Navbar/Navbar';
 
 export default function Header() {
-  return (
-    <>
-      <div className='h-24 flex items-center justify-center space-x-16 font-[#081C15] text-2xl'>
-        <div>Profil</div>
-        <div>Tableau de synthèse</div>
-        <div>Compétences</div>
-        <div>Expériences</div>
-        <div>Veille</div>
-        <div>Projets</div>
-        <div>Contact</div>
-        <img src={`${process.env.PUBLIC_URL}/moon.png`} alt='toogle to dark theme'></img>
-        <hr></hr>
-      </div>
-    </>
-  );
+    return (
+        <>
+            <Navbar/>
+        </>
+    );
 };
