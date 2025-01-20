1adbb946e8a21d1ec254129f7bdcb3e12be3c36d
Kevyn FLORESDACOSTA <kev.floresdacosta@ecoles-epsi.net>
2025-01-20 09:04:00 +0100

    Mise à jour du composant Navbar : ajout d'un fond blanc, modification des couleurs et ajustement de la taille du texte.

--- src/Components/Common/Navbar/Navbar.js
+++ src/Components/Common/Navbar/Navbar.js
@@ -2,19 +2,20 @@ import React from 'react';
 
 export default function Navbar() {
   return (
-    <div className="sticky top-0 h-24 flex items-center justify-between px-4 md:px-16 shadow-md">
-      <a href="/" className="text-3xl font-bold">Mon Portfolio</a>
-      <div className="hidden md:flex space-x-12">
-        <div className="text-xl">Profil</div>
-        <div className="text-xl">Tableau de synthèse</div>
-        <div className="text-xl">Compétences</div>
-        <div className="text-xl">Expériences</div>
-        <div className="text-xl">Veille</div>
-        <div className="text-xl">Projets</div>
-        <div className="text-xl">Contact</div>
+    <div className="sticky top-0 h-24 flex items-center justify-between px-4 md:px-16 bg-white shadow-md">
+      <div className="text-[#081C15] text-2xl font-bold">Mon Portfolio</div>
+      <div className="hidden md:flex space-x-8">
+        <div>Profil</div>
+        <div>Tableau de synthèse</div>
+        <div>Compétences</div>
+        <div>Expériences</div>
+        <div>Veille</div>
+        <div>Projets</div>
+        <div>Contact</div>
       </div>
-      <div className="flex items-center space-x-6">
-        <img src={`${process.env.PUBLIC_URL}/moon.png`} alt="toggle to dark theme"/>
+      <div className="flex items-center space-x-4">
+        <img src={`${process.env.PUBLIC_URL}/moon.png`} alt="toggle to dark theme" className="w-8 h-8" />
+        <button className="md:hidden text-[#081C15] text-2xl">☰</button>
       </div>
     </div>
   );
