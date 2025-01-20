9a48455747f17d8912c36ade3487b002616221e7
Kevyn FLORESDACOSTA <kev.floresdacosta@ecoles-epsi.net>
2025-01-20 18:24:48 +0100

    Mise à jour du composant TableauSynthese : suppression de l'effet de contour sur le titre et changement de la source de l'image de tableau en PNG.

--- src/pages/TableauSynthese/TableauSynthese.js
+++ src/pages/TableauSynthese/TableauSynthese.js
@@ -4,12 +4,12 @@ export default function TableauSynthese() {
   return (
     <div id='#tableau' className="container mx-auto p-4 lg:p-10">
       <div className="flex flex-col max-w-[1280px] mx-auto">
-        <h1 className="text-4xl md:text-5xl lg:text-6xl font-normal mb-6 lg:mb-12 text-center" style={{ WebkitTextStroke: '1px #081c15' }}>
+        <h1 className="text-4xl md:text-5xl lg:text-6xl font-normal mb-6 lg:mb-12 text-center">
           Tableau de synthèse
         </h1>
         <div className="flex flex-col items-center lg:items-start space-y-8 mb-8 lg:mb-0">
           <img
-            src={`${process.env.PUBLIC_URL}/tableau.pdf`}
+            src={`${process.env.PUBLIC_URL}/tableau.png`} 
             alt="Tableau de synthèse"
           />
         </div>
